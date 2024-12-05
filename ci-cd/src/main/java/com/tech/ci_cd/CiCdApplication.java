2abb0ec39f32232529e33a51d3cf1b76646b0d81
akashgwork <work.akashg@gmail.com>
2024-12-05 12:53:44 +0530

    first commit

---
+++ ci-cd/src/main/java/com/tech/ci_cd/CiCdApplication.java
@@ -0,0 +1,17 @@
+package com.tech.ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication.class, args);
+	}
+
+	//somechanges
+	//morechanges
+	//3rd changes in main
+
+}
---
+++ ci-cd/src/test/java/com/tech/ci_cd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
